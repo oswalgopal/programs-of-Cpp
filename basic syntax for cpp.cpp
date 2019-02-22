fc2b5dbf9e3708d96de2aaa06cd9663de0717f85
Gopal oswal <44470900+oswalgopal@users.noreply.github.com>
2019-02-23 00:00:39 +0530

    Create basic syntax for cpp.cpp

---
+++ basic syntax for cpp.cpp
@@ -0,0 +1,15 @@
+#include<iostream>    //This is used to add header files which contain the declaration for the identifier cout and cin  
+
+
+using namespace std; //this defines a scope for the identifiers that are used in a program (std is a namespace where the standard class libraries are defined)
+
+
+int main()
+{
+
+cout<<"This program is to understand basic syntax of c++" ;             //cout is a identifier or pre-defined object that represents the standard output in c++ 
+                                                                        // << is called a insertion or put to operator which send the contents of the variable on its right to the object on its left
+                                                                        
+
+return 0;
+}
