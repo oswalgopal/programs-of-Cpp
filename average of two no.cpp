ca393854bc74b2446a4a00f54a213f556a8b611f
Gopal oswal <44470900+oswalgopal@users.noreply.github.com>
2019-02-23 00:13:34 +0530

    Create average of two no.cpp

---
+++ average of two no.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int no_1,no_2,sum;                                        //taken two variables for no and one for the sum
+    float average;                                           //the average can be in decimal type so taken in float type
+    cout<<"enter a no";                                     //instruction for the input of the first no 
+    cin>>no_1;                                             //taking the input from the user using cin object
+    cout<<"enter another no";                             //instruction for the input of the second no
+    cin>>no_2;                                           //taking the input from the second no 
+    sum=no_1+no_2;                                      //performing the sum operation using + symbol
+    average=sum/2.0;                                   //performing the divison of sum by 2 to get the average of the two no
+    cout<<"the sum of the given no is "<<sum<<"\n";   //printing the sum of the two no   
+    cout<<"average of the given no is "<<average;    //printing the average of the two no
+    return 0;
+}
